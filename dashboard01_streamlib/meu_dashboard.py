ade6f62957766e5ac049323cf36a87bcf5e95d0d
João Pedro Torres <158365601+jpedrotorres@users.noreply.github.com>
2026-09-20 22:30:15 -0300

    dashboard

---
+++ dashboard01_streamlib/meu_dashboard.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard de Vendas", layout="wide")
+st.title('Dashboard de Vendas')
+
+@st.cache_data
+def carregar_dados():
+    df = pd.read_csv('vendas.csv', sep=';')
+    df['mes'] = df['data_hora'].str[:7]
+    return df
+
+df = carregar_dados()
+
+st.sidebar.title('Filtros')
+
+lista_de_categorias = df['categoria'].unique()
+
+categorias = st.sidebar.multiselect('Selecione as Categorias',
+    options=sorted(lista_de_categorias),
+    default=sorted(lista_de_categorias),
+)
+
+df_filtrado = df[df['categoria'].isin(categorias)]
+
+receita_calculada = df_filtrado['valor_venda'].sum()
+total_pedidos = len(df_filtrado)
+
+col1, col2 = st.columns([1,1])
+with col1:
+    st.metric(label='Receita Total', value=f"R$ {receita_calculada:,.2f}")
+
+with col2:
+    st.metric(label='Total de Pedidos', value=total_pedidos)
+
+aba1, aba2 = st.tabs(['Evolução Mensal', 'Tabela de Dados'])
+
+with aba1:
+    dados_agrupados = df_filtrado.groupby('mes')['valor_venda'].sum()
+    st.area_chart(dados_agrupados)
+
+with aba2:
+    st.dataframe(df_filtrado)
+
+    st.download_button(
+        label="Baixar Tabela em CSV",
+        data=df_filtrado.to_csv(index=False).encode('utf-8'),
+        file_name="vendas_filtradas.csv",
+        mime="text/csv"
+    )
